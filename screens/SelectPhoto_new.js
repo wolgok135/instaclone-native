3adbbc7ca6bb07ea5d022d9fa0a7f0ee760ad0cd
wolgok135 <wolgok135@naver.com>
2023-07-06 23:03:44 +0900

    problem occurs and change my env

--- screens/SelectPhoto_new.js
+++ screens/SelectPhoto_new.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
-
 import * as MediaLibrary from "expo-media-library";
-
 import styled from "styled-components/native";
-import { Linking } from "react-native";
 
 const Container = styled.View`
   flex: 1;
@@ -22,49 +19,35 @@ const Bottom = styled.View`
 
 export default function SelectPhoto() {
   const [ok, setOk] = useState(false);
-
+  const [photos, setPhotos] = useState([]);
+  const getPhotos = async () => {
+    if (ok) {
+      const { assets: photos } = await MediaLibrary.getAssetsAsync();
+      //const albums = await MediaLibrary.getAlbumsAsync();
+      console.log(photos);
+      setPhotos(photos);
+    }
+  };
   const getPermissions = async () => {
-    //const permissions = await MediaLibrary.getPermissionsAsync();
     const { accessPrivileges, canAskAgain } =
       await MediaLibrary.getPermissionsAsync();
-    console.log("ap", accessPrivileges);
-    console.log("ca", canAskAgain);
-
-    // accessPrivileges가 none이라는 것은 유저가 권한을 거절했거나, 아직 요청하지 않았을 때임.
     if (accessPrivileges === "none" && canAskAgain) {
       const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
       if (accessPrivileges !== "none") {
-        console.log("1", accessPrivileges);
         setOk(true);
       }
     } else if (accessPrivileges !== "none") {
-      console.log("2", accessPrivileges);
       setOk(true);
     }
   };
-  const getPhotos = async () => {
-    console.log("ok", ok);
-
-    if (ok) {
-      try {
-        console.log("3");
-        const albums = await MediaLibrary.getAlbumsAsync();
-        console.log(albums);
-      } catch (err) {
-        console.log("error", err);
-      }
-    }
-  };
-
   useEffect(() => {
     getPermissions();
     getPhotos();
-  }, [ok]);
-
+  }, []);
   return (
     <Container>
       <Top />
-      <Bottom />
+      <Bottom></Bottom>
     </Container>
   );
 }
